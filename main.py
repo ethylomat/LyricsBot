487d8d9d7ffb76b724bd9a6502497f1c1f9e4c02
Lennart S <ethylomat@c6h12o6.de>
2017-08-30 23:49:17 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import os
+from PyLyrics import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    a=os.popen("./SpotifyControl/SpotifyControl info").read().split("\n")
+    artist = a[1].replace(" Artist:   ", "")
+    track = a[2].replace(" Track:    ", "")
+    album = a[3].replace(" Album:    ", "")
+    try:
+        lyrics = PyLyrics.getLyrics(artist,track)
+        pass
+    except:
+        lyrics = "No lyrics found"
+        pass
+    return render_template("index.html", artist = artist, track = track, album = album, lyrics = lyrics.replace("\n", "<br>"))
